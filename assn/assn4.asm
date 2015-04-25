5df927d04053b8710ddafbb899a87469738d480a
plusEight <cfan002@ucr.edu>
2015-04-24 23:05:15 -0700

    set up  basic parsing framework for assn4

--- assn/assn4.asm
+++ assn/assn4.asm
@@ -24,33 +24,62 @@
 ;INSERT CODE STARTING FROM HERE 
 ;--------------------------------
 ;TO Output Intro Message
+VERYBEGIN
+;NOTE::::::: Don't Forget to check for + - after first char!!!!!!
+LD R6, ZERO
 LD R0, introMessage  ; Output Intro Message
+PUTS
+
+LEA R0, NEWLINE
 PUTS
 
 BEGIN
 	GETC
 	OUT
-	
-  LD R5, NEGATIVE
+
+  LD R5, NEGATIVE ; Check if -
 		NOT R5, R5
 		ADD R5, R5, #1
 
-		ADD R5, R5, #0
-	BRz negINPUT
+		ADD R5, R5, R0
+		BRz negINPUT
 
-	LD R5, POSITIVE
+	LD R5, POSITIVE ;Check if +
 		NOT R5, R5
 		ADD R5, R5, #1
 
-		ADD R5, R5, #0
-	BRz posINPUT
-
+		ADD R5, R5, R0
+		
 	
-	LOOPINPUT
+	NUMBERSONLY
+		LD R5, negASCII ;check for non-numerical
+			ADD R5, R5, R0
+			BRn INVALID1
+		LD R5, posASCII ;^ (after 57 ascii)
+			ADD R5, R5, R0
+			BRp INVALID1
+		LD R5, ENTER ;check for enter
+			ADD R5, R5, R0
+			BRz	ENDPROG
+
 		GETC
 		OUT
-	ENDINPUT
+	BR NUMBERSONLY	
+	END_INPUTLOOP
+	
+	STARTCONV
 
+	INVALID1
+		LEA R0, NEWLINE
+		PUTS
+		LD R0, errorMessage
+		PUTS
+		BR VERYBEGIN
+	negINPUT
+		ADD R6, R6, #1
+		BR NUMBERSONLY
+
+ENDPROG
 HALT
 ;---------------	
 ;Data
@@ -62,6 +91,11 @@ errorMessage .FILL x6100
 ZERO .FILL #0
 NEGATIVE .FILL #45
 POSITIVE .FILL #43
+NEWLINE .STRINGZ "\n"
+CHECKNUM .FILL #9
+negASCII .FILL #-48
+posASCII .FILL #-58
+ENTER .FILL #-10
 ;------------
 ;Array
 ;------------
