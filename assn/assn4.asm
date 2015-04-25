65dda830a094ce0829b181c5acaf6642ccd3f266
plusEight <cfan002@ucr.edu>
2015-04-24 17:58:21 -0700

    implemented basic structure for assn4

--- assn/assn4.asm
+++ assn/assn4.asm
@@ -1,12 +1,12 @@
 ;=================================================
 ; 
-; Name: <last name, first name>
-; Username:
-;
-; SID:
-; Assignment name: <assn ?>
-; Lab section: 
-; TA: 
+; Name: Chuanping Fan
+; Username: cfan002@ucr.edu
+;	
+; SID: 861105608
+; Assignment name: assn 4
+; Lab section: 029
+; TA: Jose
 ;
 ; I hereby certify that I have not received 
 ; assistance on this assignment, or used code
@@ -27,19 +27,45 @@
 LD R0, introMessage  ; Output Intro Message
 PUTS
 
-;TO Output Error Message
-LD R0, errorMessage  ; Output Error Message
-PUTS
+BEGIN
+	GETC
+	OUT
+	
+  LD R5, NEGATIVE
+		NOT R5, R5
+		ADD R5, R5, #1
+
+		ADD R5, R5, #0
+	BRz negINPUT
+
+	LD R5, POSITIVE
+		NOT R5, R5
+		ADD R5, R5, #1
+
+		ADD R5, R5, #0
+	BRz posINPUT
+
+	
+	LOOPINPUT
+		GETC
+		OUT
+	ENDINPUT
 
 HALT
 ;---------------	
 ;Data
 ;---------------
 
-
+arraybegin .FILL x4000
 introMessage .FILL x6000
 errorMessage .FILL x6100
-
+ZERO .FILL #0
+NEGATIVE .FILL #45
+POSITIVE .FILL #43
+;------------
+;Array
+;------------
+.ORIG x4000
 ;------------
 ;Remote data
 ;------------
@@ -53,7 +79,6 @@ intro .STRINGZ	"Input a positive or negative decimal number (max 5 digits), foll
 ;---------------
 .ORIG x6100	
 error_mes .STRINGZ	"ERROR INVALID INPUT\n"
-
 ;---------------
 ;END of PROGRAM
 ;---------------
